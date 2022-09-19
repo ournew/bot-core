5ae658f7308fb9ba017653ed9c44651b1b7f3dba
Luca Patera <lucapatera@outlook.it>
2022-09-19 18:46:48 +0200

    Add "stats" helper + "message" helper refactoring

--- helpers/utilities.php
+++ helpers/utilities.php
@@ -2,6 +2,10 @@
 
 namespace OurNew\BotCore\Helpers;
 
+use Illuminate\Support\Str;
+use OurNew\BotCore\Models\Statistic;
+use SergiX44\Nutgram\Nutgram;
+
 /**
  * Render an HTML message
  * @param string $view
@@ -10,16 +14,25 @@ namespace OurNew\BotCore\Helpers;
  */
 function message(string $view, array $values = []): string
 {
-    return rescue(function () use ($view, $values) {
-        //render view
-        $message = view("bot-core::messages.$view", $values)->render();
-        
-        //remove line breaks
-        $message = preg_replace('/\r\n|\r|\n/', '', $message);
-        
-        //replace <br> with \n
-        $message = str_replace(['<br>', '</br>', '<BR>', '</BR>'], "\n", $message);
-        
-        return $message;
-    }, 'messages.' . $view, true);
+    return rescue(fn() => Str::of(view("bot-core::messages.$view", $values)->render())
+        ->replaceMatches('/\r\n|\r|\n/', '')
+        ->replace(['<br>', '</br>', '<BR>', '</BR>'], "\n")
+        ->toString(), 'messages.' . $view, true);
+}
+
+/**
+ * Save bot statistic
+ * @param string $action
+ * @param string|null $category
+ * @param array|null $value
+ * @param int|null $chat_id
+ */
+function stats(string $action, string $category = null, array $value = null, int $chat_id = null): void
+{
+    Statistic::create([
+        'action' => $action,
+        'category' => $category,
+        'value' => $value,
+        'chat_id' => $chat_id ?? app(Nutgram::class)->userId(),
+    ]);
 }
